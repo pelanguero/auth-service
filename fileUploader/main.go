c5d7e538c05d1b8a4431444be7ce34c3a6877898
pelanguero <pelanguero@gmail.com>
2020-11-02 03:24:12 -0500

    coneccion a mongo

--- fileUploader/main.go
+++ fileUploader/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 	"time"
 )
 
@@ -15,6 +16,7 @@ func uploadFile(w http.ResponseWriter, r *http.Request) {
 	file, handler, err := r.FormFile("myFile")
 	id := r.Form.Get("id")
 	fmt.Println(id)
+	fmt.Println(formatRequest(r))
 	if err != nil {
 		fmt.Println("Error Retrieving the File")
 		fmt.Println(err)
@@ -51,4 +53,30 @@ func setupRoutes() {
 func main() {
 	fmt.Println("Hello World")
 	setupRoutes()
+}
+
+func formatRequest(r *http.Request) string {
+	// Create return string
+	var request []string
+	// Add the request string
+	url := fmt.Sprintf("%v %v %v", r.Method, r.URL, r.Proto)
+	request = append(request, url)
+	// Add the host
+	request = append(request, fmt.Sprintf("Host: %v", r.Host))
+	// Loop through headers
+	for name, headers := range r.Header {
+		name = strings.ToLower(name)
+		for _, h := range headers {
+			request = append(request, fmt.Sprintf("%v: %v", name, h))
+		}
+	}
+
+	// If this is a POST, add post data
+	if r.Method == "POST" {
+		r.ParseForm()
+		request = append(request, "\n")
+		request = append(request, r.Form.Encode())
+	}
+	// Return the request as a string
+	return strings.Join(request, "\n")
 }
--- ginserver.go
+++ ginserver.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+type Usuario struct {
+	ID     primitive.ObjectID
+	Nombre string
+	Correo string
+	Foto   string
+	Clave  string
+}
 
 func main() {
 	router := gin.Default()
@@ -18,6 +31,8 @@ func main() {
 	})
 	router.POST("/upload", upload)
 	router.StaticFS("/file", http.Dir("public"))
+	router.GET("/usuarios/", handleGetUsers)
+	router.PUT("/usuarios/", handleCreateUser)
 	router.Run(":8080")
 }
 
@@ -42,4 +57,70 @@ func upload(c *gin.Context) {
 	}
 	filepath := "http://localhost:8080/file/" + filename
 	c.JSON(http.StatusOK, gin.H{"filepath": filepath})
+}
+
+func mongoConnection() (*mongo.Client, context.Context, context.CancelFunc) {
+	connectTimeout := 5
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(connectTimeout)*time.Second)
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb+srv://pelanguero:halo12345@cluster0.rjmu9.gcp.mongodb.net/slice-pdf?retryWrites=true&w=majority"))
+	if err != nil {
+		log.Printf("Fallo al crear el cliente: %v", err)
+	}
+
+	if err != nil {
+		log.Printf("Failed to connect to cluster: %v", err)
+	}
+
+	// Force a connection to verify our connection string
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Printf("Failed to ping cluster: %v", err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+	return client, ctx, cancel
+}
+
+func Create(user *Usuario) (primitive.ObjectID, error) {
+	client, ctx, cancel := mongoConnection()
+	defer cancel()
+	defer client.Disconnect(ctx)
+	user.ID = primitive.NewObjectID()
+
+	result, err := client.Database("slice-pdf").Collection("test1").InsertOne(ctx, user)
+	if err != nil {
+		log.Printf("Could not create Task: %v", err)
+		return primitive.NilObjectID, err
+	}
+	oid := result.InsertedID.(primitive.ObjectID)
+	return oid, nil
+}
+
+func handleCreateUser(c *gin.Context) {
+	var userr Usuario
+	err := c.ShouldBindJSON(&userr)
+	if err != nil {
+		log.Print(err)
+		log.Print(userr.Nombre)
+		c.JSON(http.StatusBadRequest, gin.H{"msg": err})
+		return
+	}
+	id, err := Create(&userr)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"msg": err})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"id": id})
+}
+
+func handleGetUsers(c *gin.Context) {
+	var tasks []Usuario
+	var task Usuario
+	task.Nombre = "pelanguero"
+	task.Correo = "pelanguero@gmail.com"
+
+	tasks = append(tasks, task)
+	c.JSON(http.StatusOK, gin.H{"tasks": tasks})
 }
